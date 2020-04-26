ed9509970b4e468a871178bc6e341b9c2dd1b102
Paul Halliday <hello@paulhalliday.io>
2020-04-26 18:31:26 +0100

    Updates

--- lib/presentation/tabs/models/tab_navigation_item.dart
+++ lib/presentation/tabs/models/tab_navigation_item.dart
@@ -1,5 +1,7 @@
-/// lib/presentation/tabs/models/tab_navigation_item.dart
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping/presentation/home/pages/home_page.dart';
+import 'package:flutter_shopping/presentation/search/pages/search_page.dart';
+import 'package:flutter_shopping/presentation/shop/pages/shop_page.dart';
 
 class TabNavigationItem {
   final Widget page;
@@ -11,4 +13,22 @@ class TabNavigationItem {
     @required this.title,
     @required this.icon,
   });
+
+  static List<TabNavigationItem> get items => [
+        TabNavigationItem(
+          page: HomePage(),
+          icon: Icon(Icons.home),
+          title: Text("Home"),
+        ),
+        TabNavigationItem(
+          page: ShopPage(),
+          icon: Icon(Icons.shopping_basket),
+          title: Text("Shop"),
+        ),
+        TabNavigationItem(
+          page: SearchPage(),
+          icon: Icon(Icons.search),
+          title: Text("Search"),
+        ),
+      ];
 }
--- lib/presentation/tabs/pages/tabs_page.dart
+++ lib/presentation/tabs/pages/tabs_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shopping/presentation/home/pages/home_page.dart';
-import 'package:flutter_shopping/presentation/search/pages/search_page.dart';
-import 'package:flutter_shopping/presentation/shop/pages/shop_page.dart';
 import 'package:flutter_shopping/presentation/tabs/models/tab_navigation_item.dart';
 
 class TabsPage extends StatefulWidget {
@@ -10,24 +7,6 @@ class TabsPage extends StatefulWidget {
 }
 
 class _TabsPageState extends State<TabsPage> {
-  final List<TabNavigationItem> pages = [
-    TabNavigationItem(
-      page: HomePage(),
-      icon: Icon(Icons.home),
-      title: Text("Home"),
-    ),
-    TabNavigationItem(
-      page: ShopPage(),
-      icon: Icon(Icons.shopping_basket),
-      title: Text("Shop"),
-    ),
-    TabNavigationItem(
-      page: SearchPage(),
-      icon: Icon(Icons.search),
-      title: Text("Search"),
-    ),
-  ];
-
   int _currentIndex = 0;
 
   @override
@@ -36,14 +15,14 @@ class _TabsPageState extends State<TabsPage> {
       body: IndexedStack(
         index: _currentIndex,
         children: <Widget>[
-          for (final tabItem in pages) tabItem.page,
+          for (final tabItem in TabNavigationItem.items) tabItem.page,
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (int index) => setState(() => _currentIndex = index),
         items: <BottomNavigationBarItem>[
-          for (final tabItem in pages)
+          for (final tabItem in TabNavigationItem.items)
             BottomNavigationBarItem(
               icon: tabItem.icon,
               title: tabItem.title,
